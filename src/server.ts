75454e1106c0398c68ecbef782f499c141f77137
unknown <you@example.com>
2024-06-21 23:52:41 +0600

    initialize the project

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import config from "./app/config";
+
+const mongoose = require('mongoose');
+
+
+async function main() {
+    await mongoose.connect(config.database_url);
+}
+
+
+
+app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`)
+  })
